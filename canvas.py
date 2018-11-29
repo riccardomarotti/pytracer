8b180304137ef0781b286da59d4e3ee976daae2a
Riccardo Marotti <riccardo.marotti@gmail.com>
2018-11-29 21:31:32 +0100

    Add Canvas to PPM conversion

--- canvas.py
+++ canvas.py
@@ -2,9 +2,30 @@ import numpy as np
 
 
 class Canvas:
-    def __init__(self, width, height):
+    def __init__(self, width, height, pixels=None):
         self.width = width
         self.height = height
+        self.px = pixels
+
+        if pixels is None:
+            self.px = np.zeros((width, height, 3))
 
     def pixels(self):
-        return np.zeros((self.height, self.width, 3))
+        return self.px
+
+    def PPM(self):
+        ppm_string = "P3\n{} {}\n255\n".format(self.width, self.height)
+
+        for row_id in range(self.height):
+            colors = self.px[:, row_id]
+            colors = colors.flatten()
+            clamped_colors = clamp(colors)
+            ppm_string += " ".join(clamped_colors.astype(str))
+            ppm_string += "\n"
+
+        return ppm_string
+
+
+@np.vectorize
+def clamp(color):
+    return int(max(0, min(255, color * 255)))
--- test_canvas.py
+++ test_canvas.py
@@ -9,3 +9,35 @@ def test_creating_canvas():
     assert(c.width == 10)
     assert(c.height == 20)
     assert(c.pixels().all() == expected_pixels.all())
+
+
+def test_constructing_the_PPM_header():
+    c = Canvas(5, 3)
+
+    expectedPPM = """P3
+5 3
+255"""
+    assert(c.PPM().startswith(expectedPPM))
+
+
+def test_constructing_the_PPM_pixel_data():
+    c1 = [1.5, 0, 0]
+    c2 = [0, 0.5, 0]
+    c3 = [-0.5, 0, 1]
+
+    pixels = np.zeros((5, 3, 3))
+    pixels[0, 0] = c1
+    pixels[2, 1] = c2
+    pixels[4, 2] = c3
+
+    c = Canvas(5, 3, pixels)
+
+    expectedPPM = """P3
+5 3
+255
+255 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 127 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 255
+"""
+
+    assert(expectedPPM == c.PPM())
