d70ffdf0facd9d3a5d460eee77903e738c47a7fc
Riccardo Marotti <riccardo.marotti@gmail.com>
2018-11-30 15:13:55 +0100

    Use better syntax for transormations

--- test_transofrmations.py
+++ test_transofrmations.py
@@ -1,4 +1,5 @@
 import transformations
+from transformations import identity
 from tuples import point, vector
 import numpy as np
 from numpy.linalg import inv
@@ -9,45 +10,45 @@ def test_multiplying_by_a_translation_matrix():
     transform = transformations.translation(5, -3, 2)
     p = point(-3, 4, 5)
 
-    assert(point(2, 1, 7).all() == transform.dot(p).all())
+    assert(point(2, 1, 7).all() == transform(p).all())
 
 
 def test_multiplying_by_the_inverse_of_a_translation_matrix():
     transform = transformations.translation(5, -3, 2)
-    transform = inv(transform)
+    transform = transformations.invert(transform)
     p = point(-3, 4, 5)
 
-    assert(point(-8, 7, 3).all() == transform.dot(p).all())
+    assert(point(-8, 7, 3).all() == transform(p).all())
 
 
 def test_translation_does_not_affect_vectors():
     transform = transformations.translation(5, -3, 2)
     v = vector(-3, 4, 5)
 
-    assert(v.all() == transform.dot(v).all())
+    assert(v.all() == transform(v).all())
 
 
 def test_a_scaling_matrix_applied_to_a_point():
     transform = transformations.scaling(2, 3, 4)
     p = point(-4, 6, 8)
 
-    assert(point(-8, 18, 32).all() == (transform.dot(p)).all())
+    assert(point(-8, 18, 32).all() == (transform(p)).all())
 
 
 def test_a_scaling_matrix_applied_to_a_vecor():
     transform = transformations.scaling(2, 3, 4)
     v = vector(-4, 6, 8)
 
-    assert(vector(-8, 18, 32).all() == (transform.dot(v)).all())
+    assert(vector(-8, 18, 32).all() == (transform(v)).all())
 
 
 def test_multiplying_the_inverse_of_a_scaling_matrix():
     transform = transformations.scaling(2, 3, 4)
-    inverse = inv(transform)
+    inverse = transformations.invert(transform)
     v = vector(-4, 6, 8)
 
     expected = vector(-2, 2, 2)
-    actual = inverse.dot(v)
+    actual = inverse(v)
     assert(expected.all() == actual.all())
 
 
@@ -55,7 +56,7 @@ def test_reflection_is_scaling_by_a_negative_value():
     transform = transformations.scaling(-1, 1, 1)
     p = point(2, 3, 4)
 
-    assert(point(-2, 3, 4).all() == (transform.dot(p)).all())
+    assert(point(-2, 3, 4).all() == (transform(p)).all())
 
 
 def test_rotating_a_point_around_the_x_axis():
@@ -64,17 +65,17 @@ def test_rotating_a_point_around_the_x_axis():
     full_quarter = transformations.rotation_x(math.pi/2)
 
     assert(point(0, math.sqrt(2)/2, math.sqrt(2)/2).all()
-           == half_quarter.dot(p).all())
-    assert(point(0, 0, 1).all() == full_quarter.dot(p).all())
+           == half_quarter(p).all())
+    assert(point(0, 0, 1).all() == full_quarter(p).all())
 
 
 def test_the_inverse_of_an_x_rotation_rotates_in_the_opposite_direction():
     p = point(0, 1, 0)
     half_quarter = transformations.rotation_x(math.pi/4)
-    inverse = inv(half_quarter)
+    inverse = transformations.invert(half_quarter)
 
     assert(point(0, math.sqrt(2)/2, -math.sqrt(2)/2).all()
-           == inverse.dot(p).all())
+           == inverse(p).all())
 
 
 def test_rotating_a_point_around_the_y_axis():
@@ -83,8 +84,8 @@ def test_rotating_a_point_around_the_y_axis():
     full_quarter = transformations.rotation_y(math.pi/2)
 
     assert(point(-math.sqrt(2)/2, 0, math.sqrt(2)/2).all()
-           == half_quarter.dot(p).all())
-    assert(point(1, 0, 0).all() == full_quarter.dot(p).all())
+           == half_quarter(p).all())
+    assert(point(1, 0, 0).all() == full_quarter(p).all())
 
 
 def test_rotating_a_point_around_the_z_axis():
@@ -93,38 +94,38 @@ def test_rotating_a_point_around_the_z_axis():
     full_quarter = transformations.rotation_z(math.pi/2)
 
     assert(point(-math.sqrt(2)/2, math.sqrt(2)/2, 0).all()
-           == half_quarter.dot(p).all())
-    assert(point(-1, 0, 0).all() == full_quarter.dot(p).all())
+           == half_quarter(p).all())
+    assert(point(-1, 0, 0).all() == full_quarter(p).all())
 
 
 def test_a_shearing_transformation_moves_x_in_proportion_to_y():
     transform = transformations.shearing(1, 0, 0, 0, 0, 0)
-    assert(point(5, 3, 4).all() == transform.dot(point(2, 3, 4)).all())
+    assert(point(5, 3, 4).all() == transform(point(2, 3, 4)).all())
 
 
 def test_a_shearing_transformation_moves_x_in_proportion_to_z():
     transform = transformations.shearing(0, 1, 0, 0, 0, 0)
-    assert(point(6, 3, 4).all() == transform.dot(point(2, 3, 4)).all())
+    assert(point(6, 3, 4).all() == transform(point(2, 3, 4)).all())
 
 
 def test_a_shearing_transformation_moves_y_in_proportion_to_x():
     transform = transformations.shearing(0, 0, 1, 0, 0, 0)
-    assert(point(2, 5, 4).all() == transform.dot(point(2, 3, 4)).all())
+    assert(point(2, 5, 4).all() == transform(point(2, 3, 4)).all())
 
 
 def test_a_shearing_transformation_moves_y_in_proportion_to_z():
     transform = transformations.shearing(0, 0, 0, 1, 0, 0)
-    assert(point(2, 7, 4).all() == transform.dot(point(2, 3, 4)).all())
+    assert(point(2, 7, 4).all() == transform(point(2, 3, 4)).all())
 
 
 def test_a_shearing_transformation_moves_z_in_proportion_to_x():
     transform = transformations.shearing(0, 0, 0, 0, 1, 0)
-    assert(point(2, 3, 6).all() == transform.dot(point(2, 3, 4)).all())
+    assert(point(2, 3, 6).all() == transform(point(2, 3, 4)).all())
 
 
 def test_a_shearing_transformation_moves_z_in_proportion_to_y():
     transform = transformations.shearing(0, 0, 0, 0, 0, 1)
-    assert(point(2, 3, 7).all() == transform.dot(point(2, 3, 4)).all())
+    assert(point(2, 3, 7).all() == transform(point(2, 3, 4)).all())
 
 
 def test_individual_transformations_are_applied_in_sequence():
@@ -133,13 +134,13 @@ def test_individual_transformations_are_applied_in_sequence():
     B = transformations.scaling(5, 5, 5)
     C = transformations.translation(10, 5, 7)
 
-    p2 = A.dot(p)
+    p2 = A(p)
     assert(np.allclose(point(1, -1, 0), p2))
 
-    p3 = B.dot(p2)
+    p3 = B(p2)
     assert(np.allclose(point(5, -5, 0), p3))
 
-    p4 = C.dot(p3)
+    p4 = C(p3)
     assert(np.allclose(point(15, 0, 7), p4))
 
 
@@ -149,6 +150,6 @@ def test_chained_transofrmations_must_be_applied_in_reverse_order():
     B = transformations.scaling(5, 5, 5)
     C = transformations.translation(10, 5, 7)
 
-    T = C.dot(B).dot(A)
+    p_transformed = C(B(A(p)))
 
-    assert(np.allclose(point(15, 0, 7), T.dot(p)))
+    assert(np.allclose(point(15, 0, 7), p_transformed))
--- transformations.py
+++ transformations.py
@@ -2,55 +2,76 @@ import numpy as np
 import math
 
 
-def translation(x, y, z):
-    return np.array([
-        [1, 0, 0, x],
-        [0, 1, 0, y],
-        [0, 0, 1, z],
-        [0, 0, 0, 1]
-    ])
-
-
-def scaling(x, y, z):
-    return np.array([
-        [x, 0, 0, 0],
-        [0, y, 0, 0],
-        [0, 0, z, 0],
-        [0, 0, 0, 1]
-    ])
-
-
-def rotation_x(alpha):
+def identity():
     return np.array([
         [1, 0, 0, 0],
-        [0, math.cos(alpha), -math.sin(alpha), 0],
-        [0, math.sin(alpha), math.cos(alpha), 0],
-        [0, 0, 0, 1]
-    ])
-
-
-def rotation_y(alpha):
-    return np.array([
-        [math.cos(alpha), 0, math.sin(alpha), 0],
         [0, 1, 0, 0],
-        [-math.sin(alpha), 0, math.cos(alpha), 0],
-        [0, 0, 0, 1]
-    ])
-
-
-def rotation_z(alpha):
-    return np.array([
-        [math.cos(alpha), -math.sin(alpha), 0, 0],
-        [math.sin(alpha), math.cos(alpha), 0, 0],
         [0, 0, 1, 0],
         [0, 0, 0, 1]
     ])
 
 
+def translation(x, y, z):
+    T = np.array([
+        [1, 0, 0, x],
+        [0, 1, 0, y],
+        [0, 0, 1, z],
+        [0, 0, 0, 1]
+    ])
+    return lambda p: T.dot(p)
+
+
+def scaling(x, y, z):
+    T = np.array([
+        [x, 0, 0, 0],
+        [0, y, 0, 0],
+        [0, 0, z, 0],
+        [0, 0, 0, 1]
+    ])
+    return lambda p: T.dot(p)
+
+
+def rotation_x(alpha):
+    T = np.array([
+        [1, 0, 0, 0],
+        [0, math.cos(alpha), -math.sin(alpha), 0],
+        [0, math.sin(alpha), math.cos(alpha), 0],
+        [0, 0, 0, 1]
+    ])
+    return lambda p: T.dot(p)
+
+
+def rotation_y(alpha):
+    T = np.array([
+        [math.cos(alpha), 0, math.sin(alpha), 0],
+        [0, 1, 0, 0],
+        [-math.sin(alpha), 0, math.cos(alpha), 0],
+        [0, 0, 0, 1]
+    ])
+    return lambda p: T.dot(p)
+
+
+def rotation_z(alpha):
+    T = np.array([
+        [math.cos(alpha), -math.sin(alpha), 0, 0],
+        [math.sin(alpha), math.cos(alpha), 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ])
+    return lambda p: T.dot(p)
+
+
 def shearing(xy, xz, yx, yz, zx, zy):
-    return np.array([
+    T = np.array([
         [1, xy, xz, 0],
         [yx, 1, yz, 0],
         [zx, zy, 1, 0],
         [0, 0, 0, 1]
     ])
+    return lambda p: T.dot(p)
+
+
+def invert(t):
+    T = t(identity())
+    Tinv = np.linalg.inv(T)
+    return lambda p: Tinv.dot(p)
