38444b177852b8061a25e1bbd641681e9160dfd4
Riccardo <riccardo.marotti@gmail.com>
2018-12-01 11:40:55 +0100

    Change ray transformation implementation

--- rays.py
+++ rays.py
@@ -1,6 +1,5 @@
 import numpy as numpy
 from numba import jit
-import transformations
 
 
 @jit
@@ -8,11 +7,7 @@ def position(origin, direction, distance):
     return origin + direction*distance
 
 
-def translation(x, y, z):
-    T = transformations.translation(x, y, z)
-    return lambda origin, direction: (T(origin), T(direction))
+def apply(transform, origin, direction):
+    return transform(origin), transform(direction)
 
 
-def scaling(x, y, z):
-    T = transformations.scaling(x, y, z)
-    return lambda origin, direction: (T(origin), T(direction))
--- test_rays.py
+++ test_rays.py
@@ -1,4 +1,5 @@
 from tuples import point, vector
+import transformations
 import rays
 import numpy as np
 
@@ -18,10 +19,10 @@ def test_translating_a_ray():
     origin = point(1, 2, 3)
     direction = vector(0, 1, 0)
 
-    m = rays.translation(3, 4, 5)
-    translated_origin, translated_direction = m(origin, direction)
+    m = transformations.translation(3, 4, 5)
+    translated_origin, translated_direction = rays.apply(m, origin, direction)
 
-    assert(np.allclose(point(4,6,8), translated_origin))
+    assert(np.allclose(point(4, 6, 8), translated_origin))
     assert(np.allclose(vector(0, 1, 0), translated_direction))
 
 
@@ -29,8 +30,8 @@ def test_scaling_a_ray():
     origin = point(1, 2, 3)
     direction = vector(0, 1, 0)
 
-    m = rays.scaling(2, 3, 4)
-    scaled_origin, scaled_direction = m(origin, direction)
+    m = transformations.scaling(2, 3, 4)
+    scaled_origin, scaled_direction = rays.apply(m, origin, direction)
 
     assert(np.allclose(point(2, 6, 12), scaled_origin))
     assert(np.allclose(vector(0, 3, 0), scaled_direction))
