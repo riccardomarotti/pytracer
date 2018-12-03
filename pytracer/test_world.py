92705dcb1ecf35c944d82bf549909f9c085c8585
Riccardo Marotti <riccardo.marotti@gmail.com>
2018-12-03 17:58:57 +0100

    Add World intersection

--- pytracer/test_world.py
+++ pytracer/test_world.py
@@ -1,4 +1,6 @@
-from pytracer.world import World
+from pytracer.world import World, default_world
+from pytracer.rays import Ray
+from pytracer.tuples import point, vector
 
 
 def test_createing_a_world():
@@ -6,3 +8,16 @@ def test_createing_a_world():
 
     assert(len(w) == 0)
     assert(w.light is None)
+
+
+def test_intersect_a_world_with_a_ray():
+    w = default_world()
+    r = Ray(point(0, 0, -5), vector(0, 0, 1))
+
+    xs = w.intersect(r)
+
+    assert(len(xs) == 4)
+    assert(xs[0].t == 4)
+    assert(xs[1].t == 4.5)
+    assert(xs[2].t == 5.5)
+    assert(xs[3].t == 6)
--- pytracer/world.py
+++ pytracer/world.py
@@ -1,9 +1,16 @@
+from pytracer.lights import PointLight
+from pytracer.tuples import point, vector
+from pytracer.colors import color, white
+from pytracer.spheres import Sphere
+from pytracer.materials import Material
+from pytracer.transformations import scaling
+from pytracer.intersections import Intersection, Intersections
 
 
 class World:
-    def __init__(self):
-        self._objects = []
-        self._light = None
+    def __init__(self, light=None, *objects):
+        self._light = light
+        self._objects = objects
 
     @property
     def light(self):
@@ -11,3 +18,21 @@ class World:
 
     def __len__(self):
         return len(self._objects)
+
+    def intersect(self, ray):
+        xs = []
+        for obj in self._objects:
+            xs.extend(obj.intersect(ray))
+
+        xs.sort(key=lambda i: i.t)
+
+        return Intersections(*xs)
+
+
+def default_world():
+    light = PointLight(point(-10, 10, -10), white())
+    m = Material(color=color(0.8, 1.0, 0.6), diffuse=0.7, specular=0.2)
+    s1 = Sphere(material=m)
+    s2 = Sphere(transformation=scaling(0.5, 0.5, 0.5))
+
+    return World(list, s1, s2)
