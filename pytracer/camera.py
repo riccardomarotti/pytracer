9e74cb751830c32c23f356b5ada58dc59d136c63
Riccardo Marotti <riccardo.marotti@gmail.com>
2018-12-03 22:33:37 +0100

    Add pixel size

--- pytracer/camera.py
+++ pytracer/camera.py
@@ -1,4 +1,5 @@
 from pytracer.transformations import identity_matrix
+import math
 
 
 class Camera:
@@ -10,3 +11,19 @@ class Camera:
             self._transform = identity_matrix
         else:
             self._transform = transform
+
+        half_view = math.tan(self._field_of_view / 2)
+        aspect = self._hsize / self._vsize
+
+        if aspect >= 1:
+            self._half_width = half_view
+            self._half_height = half_view / aspect
+        else:
+            self._half_width = half_view * aspect
+            self._half_height = half_view
+
+        self._pixel_size = (self._half_width * 2) / self._hsize
+
+    @property
+    def pixel_size(self):
+        return self._pixel_size
--- tests/test_camera.py
+++ tests/test_camera.py
@@ -15,3 +15,13 @@ def test_constructing_a_camera():
     assert(c._vsize == 120)
     assert(c._field_of_view == math.pi/2)
     assert(np.array_equal(c._transform(identity_matrix()), identity_matrix()))
+
+
+def test_the_pixel_size_for_a_horizontal_canvas():
+    c = Camera(200, 125, math.pi/2)
+    assert(math.isclose(c.pixel_size, 0.01))
+
+
+def test_the_pixel_size_for_a_vertical_canvas():
+    c = Camera(125, 200, math.pi/2)
+    assert(math.isclose(c.pixel_size, 0.01))
